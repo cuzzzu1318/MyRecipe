9fe70159e75884933b801b05878c1280df1d7857
cuzzzu1318 <cuzzzu1318@naver.com>
2021-11-22 20:42:15 +0900

    adf

---
+++ category.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8"  name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no,
+maximum-scale=1.0, minimum-scale=1.0">
+    <title>카테고리 선택</title>
+    <link rel="stylesheet" href="myRecipe.css">
+  </head>
+  <body>
+    <header>
+      <img class="icon" src="image/icon_back.svg" alt="back">
+      <a href="main.php"><img class="logo" src="logo.jpg" alt="logo"></a>
+      <img class="icon" src="image/icon_user.svg" alt="user">
+    </header>
+  </body>
+</html>
