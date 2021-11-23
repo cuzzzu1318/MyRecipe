67f07194e628cb93be24d263efcd4b93db6cb9f5
cuzzzu1318 <cuzzzu1318@naver.com>
2021-11-23 20:13:05 +0900

    카테고리 출력

--- category.php
+++ category.php
@@ -1,10 +1,14 @@
-<!DOCTYPE html>
+<?php
+  $mysqli = new mysqli("localhost", "myRecipe", "pa22w0rd!", "myRecipe");
+ ?>
+ <!DOCTYPE html>
 <html lang="ko" dir="ltr">
   <head>
     <meta charset="utf-8"  name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no,
 maximum-scale=1.0, minimum-scale=1.0">
     <title>카테고리 선택</title>
     <link rel="stylesheet" href="myRecipe.css">
+    <link rel="stylesheet" href="category.css">
   </head>
   <body>
     <header>
@@ -12,5 +16,27 @@ maximum-scale=1.0, minimum-scale=1.0">
       <a href="main.php"><img class="logo" src="logo.jpg" alt="logo"></a>
       <img class="icon" src="image/icon_user.svg" alt="user">
     </header>
+    <div class="div_button_category">
+      <button type="button" class="button_category" value="전체">전체</button>
+    <?php
+      $sql = "
+          SELECT DISTINCT category FROM recipe;
+      ";
+      $result = $mysqli->query($sql);
+      if($result == false){
+        echo $mysqli->error;
+      }else{
+        if($result->num_rows > 0){
+          while($row = $result->fetch_array()){
+            $category = $row['category'];
+            ?>
+              <button type="button" class="button_category" value="<?=$category?>"><?=$category?></button>
+            <?php
+          }
+        }
+      }
+    ?>
+  </div>
+
   </body>
 </html>
