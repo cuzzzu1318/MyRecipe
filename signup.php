8d0d45b6b1b3fe28b0b36e829822d00d81a01ada
Eunjin <dmswls4634@naver.com>
2021-11-24 15:57:08 +0900

    은진

--- signup.php
+++ signup.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html lang="ko" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8"  name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no,
+maximum-scale=1.0, minimum-scale=1.0">
+    <title>MyRecipe</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="myRecipe.css">
     <link rel="stylesheet" href="signup.css">
-    <title></title>
   </head>
-  <body>
-    <img class="logo" src="logo.jpg" alt="logo">
 
-    <form class="signup_info" action="signup_process.php" method="post">
-      <input class="textbox" type="text" name="" placeholder="ID">
-      <input class="textbox" type="password" name="" placeholder="PASSWORD">
-      <input class="textbox" type="text" name="" placeholder="NICKNAME">
-      <input id="signup_button" type="button" name="" value="회원가입">
-    </form>
+  <body>
+    <header>
+      <img class="icon" src="image/icon_back.svg" alt="back">
+      <img class="logo" src="logo.jpg" alt="logo">
+    </header>
+
+    <div class="content">
+      <input type="text" id="id" placeholder="아이디를 입력해주세요">
+      <input type="password" id="pw" placeholder="비밀번호를 입력해주세요">
+      <input type="password" id="re-pw" placeholder="비밀번호를 재입력해주세요">
+      <input type="text" id="nickname" placeholder="닉네임을 입력해주세요">
+      <input type="button" id="signup" value="회원가입">
+    </div>
   </body>
 </html>
