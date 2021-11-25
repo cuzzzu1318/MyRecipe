a30a127c5c477219a8fd7e21f5a85cbb1afbe83f
cuzzzu1318 <cuzzzu1318@naver.com>
2021-11-25 16:04:33 +0900

    0

--- list.php
+++ list.php
@@ -1,3 +1,22 @@
+<?php
+  $mysqli = new mysqli("localhost", "myRecipe", "1O1o5u", "myRecipe");
+  if (isset($_GET['cur_page'])) {
+      $cur_page = $_GET['cur_page'];
+    }else {
+      $cur_page = 1;
+    }
+    $show = 10;
+    $start = (($cur_page-1)*$show);
+
+    function getTitle($title){
+      if (mb_strlen($title, "UTF-8")>10) {
+        return mb_substr($title,0, 10, "UTF-8")."...";
+      }else{
+        return $title;
+      }
+
+    }
+ ?>
 <!DOCTYPE html>
 <html lang="ko" dir="ltr">
   <head>
@@ -18,6 +37,56 @@ maximum-scale=1.0, minimum-scale=1.0">
     </header>
 
     <div class="menu_bar">
-      <input type="button" class="btn" name="공지" value="메인" style="border-bottom: 5px solid black;">
-      <input type="button" class="btn" name="게시판 보기" value="게시판 보기" onclick="location.href='category.php'">
+      <input type="button" class="btn" name="공지" value="메인"onclick="location.href='main.php'"  >
+      <input type="button" class="btn" name="게시판 보기" value="게시판 보기" style="border-bottom: 5px solid black;"onclick="location.href='category.php'" >
     </div>
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">번호</th>
+          <th scope="col">제목</th>
+          <th scope="col">비용</th>
+          <th scope="col">추천</th>
+          <th scope="col">닉네임</th>
+        </tr>
+      </thead>
+      <?php
+      $sql = "
+        SELECT * FROM recipe ORDER BY postID DESC LIMIT $start, $show ;
+      ";
+      $result = $mysqli->query($sql);
+      if ($result == false) {
+      echo $mysqli->error;
+      }else{
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_array()) {
+            $article = array(
+              'category' => $row['category'],
+              'postID' => htmlspecialchars($row['postID']),
+              'recipeName' => htmlspecialchars($row['recipeName']),
+              'cost' =>number_format(htmlspecialchars($row['cost'])),
+              'like' => htmlspecialchars($row['like']),
+              'nickname' => htmlspecialchars($row['nickname']),
+              'uploadDate' => htmlspecialchars($row['uploadDate'])
+            );
+
+          ?>
+
+          <tbody>
+           <tr style="height: 50px; cursor: pointer;" onclick="location.replace('post.php?postID=<?=$row['postID']?>');">
+             <td style="text-align: center; width: 40px; padding: 0;"><?=$article['postID']?></td>
+             <td style="width: 100px;"><?=getTitle($article['recipeName'])?></td>
+             <td>\<?=$article['cost']?></td>
+             <td style="width: 40px;"><?=$article['like']?></td>
+             <td><?=$article['nickname']?></td>
+           </tr>
+          </tbody>
+          <?php
+        }
+        }
+      }
+       ?>
+  </table>
+
+  </body>
+</html>
