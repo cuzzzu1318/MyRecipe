24e8685eff81039d949a175680c6f06ccb642f28
cuzzzu1318 <cuzzzu1318@naver.com>
2021-11-30 01:03:01 +0900

    list

--- list.php
+++ list.php
@@ -35,15 +35,62 @@ maximum-scale=1.0, minimum-scale=1.0">
     <script type="text/javascript">
      document.querySelector('#게시판').id='cur_menu';
     </script>
-    <div class="category">
-      <i class="fas fa-caret-left"></i>
-      <div class="textAndIcon">
-        <div class="text">
-          전체
-        </div>
+    <form class="" action="list.php" method="post">
+      <div class="category">
+        <select class="mr_select" id="category" onchange="submit()" name="cate">
+          <option
+          <?php if(isset($_POST['cate'])&&$_POST['cate']=='전체'){
+            echo "selected";
+          } ?>
+            value="전체"
+            >
+            전체
+          </option>
+          <option value="한식"
+          <?php if(isset($_POST['cate'])&&$_POST['cate']=='한식'){
+            echo "selected";
+          } ?>
+          >
+            한식
+          </option>
+          <option value="중식"<?php if(isset($_POST['cate'])&&$_POST['cate']=='중식'){
+            echo "selected";
+          } ?>>
+            중식
+          </option>
+          <option <?php if(isset($_POST['cate'])&&$_POST['cate']=='양식'){
+            echo "selected";
+          } ?>>
+            양식
+          </option>
+          <option <?php if(isset($_POST['cate'])&&$_POST['cate']=='일식'){
+            echo "selected";
+          } ?>>
+            일식
+          </option>
+          <option <?php if(isset($_POST['cate'])&&$_POST['cate']=='분식'){
+            echo "selected";
+          } ?>>
+            분식
+          </option>
+          <option <?php if(isset($_POST['cate'])&&$_POST['cate']=='아시안'){
+            echo "selected";
+          } ?>>
+            아시안
+          </option>
+          <option <?php if(isset($_POST['cate'])&&$_POST['cate']=='아시안'){
+            echo "selected";
+          } ?>>
+            패스트푸드
+          </option>
+          <option <?php if(isset($_POST['cate'])&&$_POST['cate']=='디저트'){
+            echo "selected";
+          } ?>>
+            디저트
+          </option>
+        </select>
       </div>
-      <i class="fas fa-caret-right"></i>
-    </div>
+    </form>
     <main class="description">
       <table>
         <thead>
@@ -55,9 +102,16 @@ maximum-scale=1.0, minimum-scale=1.0">
           </tr>
         </thead>
         <?php
-        $sql = "
-          SELECT * FROM recipe ORDER BY postID DESC LIMIT $start, $show ;
-        ";
+        if(isset($_POST['cate'])&&$_POST['cate']!='전체'){
+          $cate = $_POST['cate'];
+          $sql = "
+            SELECT * FROM recipe WHERE category = '$cate' ORDER BY postID DESC LIMIT $start, $show ;
+          ";
+        }else{
+          $sql = "
+            SELECT * FROM recipe ORDER BY postID DESC LIMIT $start, $show ;
+          ";
+        }
         $result = $mysqli->query($sql);
         if ($result == false) {
         echo $mysqli->error;
@@ -90,6 +144,61 @@ maximum-scale=1.0, minimum-scale=1.0">
         }
          ?>
     </table>
+
+  <div id="page">
+    <div id="prev">
+      <?php
+      if ($cur_page>1) {
+        $prev_page = $cur_page-1;
+        echo "
+        <a href=\"list.php?cur_page=$prev_page\">이전</a>
+        ";
+      }
+      ?>
+    </div>
+    <div id="pages">
+      <?php
+      if(isset($_POST['cate'])&&$_POST['cate']!='전체'){
+        $cate = $_POST['cate'];
+        $sql = "
+          SELECT COUNT(*) FROM recipe WHERE category = '$cate';
+        ";
+      }else{
+        $sql = "
+          SELECT COUNT(*) FROM recipe;
+        ";
+      }
+      $result = $mysqli->query($sql);
+      $row = $result->fetch_array();
+      $page = 1;
+      for ($count=0; $count < $row[0]; $count++) {
+        if ($count%10==1) {
+          if ($cur_page==$page) {
+            echo "
+            <a class=\"pagenum\" href=\"list.php?cur_page=$page\">$page</a>
+            ";
+          }
+          else {
+            echo "
+            <a class=\"pagenum\" href=\"list.php?cur_page=$page\">$page</a>
+            ";
+          }
+          $page++;
+        }
+      }
+      ?>
+    </div>
+    <div id="next">
+      <?php
+      if ($cur_page<$page-1) {
+        $next_page = $cur_page+1;
+        echo "
+        <a href=\"list.php?cur_page=$next_page\">다음</a>
+        ";
+      }
+       ?>
+    </div>
+  </div>
     </main>
   <img src="image/button_plus.png" onclick="location.href='write.php'" id="write" alt="">
   </body>
